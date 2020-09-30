daef373d6b3b56f922daacc08e3f600d26bdf608
Daniyal Marofi <daniyaleight8@gmail.com>
2020-09-30 12:47:39 +0330

    added start_day and note input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int main()
-{
-    cout << "Hello Wolrd!" << endl;
+struct note {
+    string date;
+    string text;
+};
+
+string get_note(string user_input) {
+    string temp;
+    getline(cin, temp);
+    return user_input + temp;
+}
+
+int main() {
+    string userinput;
+    string today = "";
+    vector<note> notes;
+
+    while (cin >> userinput) {
+        if (!userinput.compare("start_day")) {
+            char temp;
+            // get today
+            cin >> today;
+        } else {
+            string text = get_note(userinput);
+            note temp;
+            temp.date = today;
+            temp.text = text;
+            notes.push_back(temp);
+        }
+    }
 
     return 0;
 }
