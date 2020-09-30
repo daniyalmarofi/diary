d01aa663c6ed93d73e925e05b202718452d7efc8
Daniyal Marofi <daniyaleight8@gmail.com>
2020-09-30 13:20:55 +0330

    added show summary

--- main.cpp
+++ main.cpp
@@ -10,11 +10,22 @@ struct note {
 };
 
 //** this function gets the other part of note and concatenates with the first
-//part of note
+// part of note
 string get_note(string user_input) {
     string temp;
     getline(cin, temp);
     return user_input + temp;
+}
+
+//** this function shows the summary of a note.
+void show_summary(note note) {
+    cout << note.date << endl;
+    if (note.text.length() < 20)
+        cout << note.text << endl;
+    else {
+        for (int i = 0; i < 20; i++) cout << note.text[i];
+        cout << "..." << endl;
+    }
 }
 
 int main() {
@@ -31,10 +42,8 @@ int main() {
             string date;
             // get the date that user wants to see
             cin >> date;
-            for (auto note : notes) {
-                if(!note.date.compare(date))
-                    cout<<note.text<<endl;
-            }
+            for (auto note : notes)
+                if (!note.date.compare(date)) cout << note.text << endl;
         } else {
             // user wants to add a new note so we push back the note to the
             // notes vector
